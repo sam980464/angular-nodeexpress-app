97fcac5d42ab8db3b054cf24b0bc1f8cc54d2bd6
Suman Das <sdas148@gmail.com>
2019-02-13 22:21:29 +0530

    Update Sql Connectivity

--- server.js
+++ server.js
@@ -14,4 +14,58 @@ res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
+
+
+const mysql = require('mysql');
+const dbconfig = {
+    host     : 'us-cdbr-iron-east-03.cleardb.net',
+    user     : 'b813e86be3bf41',
+    password : '47503a61',
+    database : 'heroku_5a8c23471a3edc3'
+}
+
+
+const connection  = mysql.createConnection(dbconfig);
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+app.post("/login", (req, res, next) => {
+   
+   var username = req.body.uname;
+   var pass = req.body.password;
+   console.log(username);
+   console.log(pass);
+   connection.query("select * from users where username='"+username + "' and password='"+pass+"' ",(err,response)=>{
+	   if (err) throw err;
+	   if(response){
+		res.send(response);	
+	   }else{
+		   res.send("{}");
+	   }
+	   
+   })
+  
+});
+
+
+app.post("/signup", (req, res, next) => {
+   
+   var email  = req.body.email;
+   var mobile = req.body.mobile;
+   var uname  = req.body.uname;
+   var pass   = req.body.password;
+   connection.query("insert into users (email,mobi_no,username,password) values('"+email+"','"+mobile+"','"+uname+"','"+pass+"')",(err,response)=>{
+	   if (err) throw err;
+	   if(response){
+		res.send(response);	
+	   }else{
+		   res.send("{}");
+	   }
+	   
+   })
+  
+});
+
+
 app.listen(process.env.PORT || 8080);
