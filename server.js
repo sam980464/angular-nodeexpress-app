35295b311511622ae25481ceab7d426f57dd3ee7
Suman Das <sdas148@gmail.com>
2019-02-15 12:50:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 //mysql://bff1ea93a381dc:5f079d48@us-cdbr-iron-east-03.cleardb.net/heroku_7e369476949ac20?reconnect=true
 
 const mysql = require('mysql');
-const dbconfig = {
+const db_config = {
     host     : 'us-cdbr-iron-east-03.cleardb.net',
     user     : 'b4e08b7f3522a3',
     password : 'f6f1b50e',
@@ -30,13 +30,28 @@ const dbconfig = {
 }
 
 
-const connection  = mysql.createConnection(dbconfig);
-connection.connect(function(err) {
-    if (err){
-		 return console.error('error: ' + err.message);
-	}
-	 console.log('Connected to the MySQL server.');
-});
+var connection ;
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+handleDisconnect();
 
 app.post("/api/login", (req, res, next) => {
    
